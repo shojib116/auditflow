ca727d50469c9ef30f057b14fc704e21b1e65bd9
Nazmul Hasan Shajib <shojib116@gmail.com>
2026-03-04 09:39:39 +0600

    feat(server): add predefined errors for consistent error message

--- server/internal/domain/iam/repository.go
+++ server/internal/domain/iam/repository.go
@@ -1,8 +1,17 @@
 package iam
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 type UserRepository interface {
 	Create(ctx context.Context, user *User) (*User, error)
 	GetUserByEmail(ctx context.Context, email Email) (*User, error)
 }
+
+var (
+	ErrUserAlreadyExists = errors.New("an user with this email already exists!")
+	ErrInvalidPassword   = errors.New("password must be at least 8 characters and include uppercase, lowercase, number, and special character")
+	ErrUserNotFound      = errors.New("no user can be found with this email!")
+)
