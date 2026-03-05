6fb7fb7696d27755343aa835051090284849dca2
Nazmul Hasan Shajib <shojib116@gmail.com>
2026-03-05 09:33:27 +0600

    feat(server): wire up repository, service and handler in main function

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/lib/pq"
 	"github.com/shojib116/auditflow-api/config"
+	iamService "github.com/shojib116/auditflow-api/internal/application/iam"
+	"github.com/shojib116/auditflow-api/internal/database"
+	iamRepository "github.com/shojib116/auditflow-api/internal/infra/iam"
+	iamHandler "github.com/shojib116/auditflow-api/internal/interfaces/http/iam"
 	"github.com/shojib116/auditflow-api/internal/interfaces/http/middlewares"
 )
 
@@ -19,13 +25,25 @@ func handlerHealth(w http.ResponseWriter, r *http.Request) {
 func main() {
 	cfg := config.GetConfig()
 
+	dbURL := database.GetConnectionString(cfg.DB)
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err.Error())
+	}
+
+	iamRepo := iamRepository.NewUserRepository(dbConn)
+	iamSvc := iamService.NewUserService(iamRepo)
+	iamHndlr := iamHandler.NewHandler(&iamSvc)
+
 	mngr := middlewares.NewManager(middlewares.Logger, middlewares.CORS(cfg.FrontendDomain))
 	mux := http.NewServeMux()
 
 	mux.Handle("GET /healthz", http.HandlerFunc(handlerHealth))
 
+	iamHndlr.RegisterRoutes(mux, mngr)
+
 	serverAddr := fmt.Sprintf(":%d", cfg.HttpPort)
-	fmt.Printf("Server running on http://localhost%v", serverAddr)
+	fmt.Printf("Server running on http://localhost%v\n", serverAddr)
 	if err := http.ListenAndServe(serverAddr, mngr.Wrap(mux)); err != nil {
 		log.Fatal("Error occured:", err)
 	}
